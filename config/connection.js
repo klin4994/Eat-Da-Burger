c429e9e0741742175cfafd2a98893efc8db4ac7b
klin4994 <kevlin129@hotmail.com>
2021-03-08 21:44:39 +1100

    Solving error

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '10.0.0.111',
   port: 3306,
   user: 'root',
   password: 'Mypassword',
