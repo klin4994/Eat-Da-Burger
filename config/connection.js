8dfd96e30613c1ff83355e1980bdb005d2bc647d
klin4994 <kevlin129@hotmail.com>
2021-03-08 21:59:57 +1100

    Solving connection error

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: 'us-cdbr-east-03.cleardb.com',
   port: 3306,
-  user: 'root',
-  password: 'Mypassword',
-  database: 'burger_db'
+  user: 'bf711484352d37',
+  password: '8fe7d9fe',
+  database: 'heroku_51b5acce9e83132'
 });
 
 // Make connection.
@@ -19,3 +19,4 @@ connection.connect((err) => {
 
 // Export connection for our ORM to use.
 module.exports = connection;
+bf711484352d37:8fe7d9fe@us-cdbr-east-03.cleardb.com/heroku_51b5acce9e83132?reconnect=true
